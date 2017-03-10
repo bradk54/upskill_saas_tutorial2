4a97301e3055f5402854285ab062108e72baf8d9
bradk54 <kittrellbm@gmail.com>
2017-03-10 00:52:53 +0000

    changed smtp settings

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,11 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => 'app64631955@heroku.com',
-  :password       => 'vbhk14fc8932',
+  :port           => ENV['587'],
+  :address        => ENV['209.61.151.224'],
+  :user_name      => ENV['postmaster@app09efe268357d469f84a57cf187edb897.mailgun.org'],
+  :password       => ENV['4287588650dce5e294e3c6312fd741ae'],
   :domain         => 'hidden-chamber-55103.herokuapp.com',
-  :enable_starttls_auto => true
+  :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
