87ef0903f371caefbba84f52f06a431c137d0de6
bradk54 <kittrellbm@gmail.com>
2017-03-11 00:37:01 +0000

    added bootstrap to registration form

--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,10 @@ require_relative 'application'
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :port           => ENV['587'],
-  :address        => ENV['209.61.151.224'],
+  :address        => ENV['smtp.mailgun.org'],
   :user_name      => ENV['postmaster@app09efe268357d469f84a57cf187edb897.mailgun.org'],
   :password       => ENV['4287588650dce5e294e3c6312fd741ae'],
-  :domain         => 'hidden-chamber-55103.herokuapp.com',
+  :domain         => 'app09efe268357d469f84a57cf187edb897.mailgun.org',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
