cf40954f581daa5b7e0291150d481de084f49c41
bradk54 <kittrellbm@gmail.com>
2017-03-16 23:58:20 +0000

    added plan vars and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
     def home
+      @basic_membershiptype = Membershiptype.find(1)
+      @pro_membershiptype = Membershiptype.find(2)  
     end
     def about
     end
-end
+end  
